73e9bc211bc307573f9f645bd63774c6ec829fef
Alex <sahok6264@gmail.com>
2022-01-26 16:51:24 +0200

    Add checkbox for enable or disable Userback widget

--- Microting.EformAngularFrontendBase/Migrations/20220126140804_AddUserbackWidgetEnableSetting.Designer.cs
+++ Microting.EformAngularFrontendBase/Migrations/20220126140804_AddUserbackWidgetEnableSetting.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Microting.EformAngularFrontendBase.Infrastructure.Data;
 
@@ -10,9 +11,10 @@ using Microting.EformAngularFrontendBase.Infrastructure.Data;
 namespace Microting.EformAngularFrontendBase.Migrations
 {
     [DbContext(typeof(BaseDbContext))]
-    partial class BaseDbContextModelSnapshot : ModelSnapshot
+    [Migration("20220126140804_AddUserbackWidgetEnableSetting")]
+    partial class AddUserbackWidgetEnableSetting
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Microting.EformAngularFrontendBase/Migrations/20220126140804_AddUserbackWidgetEnableSetting.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Microting.EformAngularFrontendBase.Migrations
+{
+    public partial class AddUserbackWidgetEnableSetting : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsUserbackEnabled",
+                table: "Users",
+                type: "tinyint(1)",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsUserbackEnabled",
+                table: "Users");
+        }
+    }
+}
