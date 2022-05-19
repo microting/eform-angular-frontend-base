dfdf1709b674fe6fbf5e447efa46c29aaa6f1ede
René Schultz Madsen <rm@microting.com>
2022-05-19 17:45:26 +0200

    Adding the new DeviceUserModel.

---
+++ Microting.EformAngularFrontendBase/Infrastructure/Data/Models/DeviceUserModel.cs
@@ -0,0 +1,10 @@
+namespace Microting.EformAngularFrontendBase.Infrastructure.Data.Models;
+
+public class DeviceUserModel
+{
+    public int Id { get; set; }
+    public string UserFirstName { get; set; }
+    public string UserLastName { get; set; }
+    public int Language { get; set; }
+    public string LanguageCode { get; set; }
+}
