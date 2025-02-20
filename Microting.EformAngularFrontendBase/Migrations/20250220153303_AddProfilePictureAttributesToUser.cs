3604efafa5c35f44521cb00dbe4c4395ab423f94
René Schultz Madsen <rm@microting.dk>
2025-02-20 16:40:49 +0100

    Adding migrations for supporting the ProfilePicture attributes.

---
+++ Microting.EformAngularFrontendBase/Migrations/20250220153303_AddProfilePictureAttributesToUser.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Microting.EformAngularFrontendBase.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddProfilePictureAttributesToUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePicture",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProfilePictureSnapshot",
+                table: "Users",
+                type: "longtext",
+                nullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProfilePicture",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "ProfilePictureSnapshot",
+                table: "Users");
+        }
+    }
+}
